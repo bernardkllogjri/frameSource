b39d39f0b192a8a0687aeba6b14aaaef2459af0e
Bernard <bernard.kllogjri@sotlab.al>
2018-10-28 15:02:05 +0100

    new change

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,11 +1,10 @@
 <?php
 
-    const __PROJECT_DIR__ = '../../../..';
     error_reporting(0);
 
     require 'helpers.php';
-    require __PROJECT_DIR__."/config/site.php";
-    require __PROJECT_DIR__."/routes/web.php";
+    require __PROJECT_DIR__.'/config/site.php';
+    require __PROJECT_DIR__.'/routes/web.php';
 
-    (new Dotenv\Dotenv(dirname(__DIR__)))->load();
+    (new Dotenv\Dotenv(__PROJECT_DIR__))->load();
     error_reporting(env('ERR_REPORTING'));
--- src/helpers.php
+++ src/helpers.php
@@ -1,67 +1,73 @@
 <?php
 
-    function env($key){
+define('__PROJECT_DIR__',
+    dirname(
+        dirname(dirname(dirname(dirname(__FILE__))))
+    )
+);
 
-        if($key == 'ERR_REPORTING'){
-            switch ($_ENV[$key]){
-                case 1:
-                    return 32759;
-                    break;
-                default:
-                    return $_ENV[$key];
-                    break;
-            }
+function env($key){
+
+    if($key == 'ERR_REPORTING'){
+        switch ($_ENV[$key]){
+            case 1:
+                return 32759;
+                break;
+            default:
+                return $_ENV[$key];
+                break;
         }
-
-        return $_ENV[$key];
     }
 
-    function formated_view_string($string, array $data = []){
-        extract($data);
-        return "../resources/views/".str_replace('.','/',$string).".view.php";
+    return $_ENV[$key];
+}
+
+function formated_view_string($string, array $data = []){
+    extract($data);
+    return "../resources/views/".str_replace('.','/',$string).".view.php";
+}
+
+function view($view, $data = []){
+    extract($data);
+    $errors = $_SESSION['errors'] ?? [];
+
+    return require formated_view_string($view);
+}
+
+function config($key){
+    $configs = require '../config/site.php';
+    return $configs[$key];
+}
+
+function dd(...$args){
+    die(var_dump($args));
+}
+
+function partials($partial){
+    include formated_view_string($partial);
+}
+
+function redirect($route){
+    return header("Location: ".env('BASE_URL')."/{$route}");
+}
+
+function auth(){
+    return !!$_SESSION['user'];
+}
+
+function back(){
+    return header("Location: {$_SERVER['HTTP_REFERER']}");
+}
+
+function errors($key=null){
+    if(!isset($key)){
+        return $_SESSION['errors'];
     }
+    $error = $_SESSION['errors'][$key];
 
-    function view($view, $data = []){
-        extract($data);
-        $errors = $_SESSION['errors'] ?? [];
-
-        return require formated_view_string($view);
+    if(!empty($error)){
+        unset($_SESSION['errors'][$key]);
+        return $error;
     }
-
-    function config($key){
-        $configs = require '../config/site.php';
-        return $configs[$key];
-    }
-
-    function dd(...$args){
-        die(var_dump($args));
-    }
-
-    function partials($partial){
-        include formated_view_string($partial);
-    }
-
-    function redirect($route){
-        return header("Location: ".env('BASE_URL')."/{$route}");
-    }
-
-    function auth(){
-        return !!$_SESSION['user'];
-    }
-
-    function back(){
-        return header("Location: {$_SERVER['HTTP_REFERER']}");
-    }
-
-    function errors($key=null){
-        if(!isset($key)){
-            return $_SESSION['errors'];
-        }
-        $error = $_SESSION['errors'][$key];
-
-        if(!empty($error)){
-            unset($_SESSION['errors'][$key]);
-            return $error;
-        }
-        return [];
-    }
+    return [];
+}
